b67471c6fc4c3748132afaceb30155df725ce2f5
Bibhas C Debnath <iambibhas@gmail.com>
2013-07-25 03:05:55 +0530

    added management command to import data

---
+++ apps/common/management/commands/import_basic.py
@@ -0,0 +1,61 @@
+import os
+import xlrd
+
+from django.core.management.base import BaseCommand, CommandError
+from common.models import Cluster, Block, EducationDistrict, Village, State
+from django.conf import settings
+
+from common.models import Cluster, Block, Village, State, EducationDistrict
+from schools.models import School
+
+class Command(BaseCommand):
+    args = '<filename filename ...>'
+    help = 'Closes the specified poll for voting'
+    INDEXES = {
+        'district': 0,
+        'school_code': 1,
+        'school_name': 2,
+        'block': 3,
+        'cluster': 4,
+        'village': 5,
+        'pincode': 6
+    }
+
+    def handle(self, *args, **options):
+        for basic_data in args:
+            full_path = os.path.join(settings.DATADUMP_ROOT, basic_data)
+            try:
+                fp = xlrd.open_workbook(full_path)
+                for sheet in fp.sheets():
+                    for idx in range(1, sheet.nrows):
+                        row = sheet.row_values(idx)
+                        if (not row[self.INDEXES['school_code']] or
+                            School.objects.filter(code=str(int(row[self.INDEXES['school_code']]))).count() > 0):
+                            continue
+
+                        district, created = EducationDistrict.objects.get_or_create(
+                            name=row[self.INDEXES['district']].capitalize()
+                        )
+                        # TODO: Detect urban/rural from village name
+                        village, created = Village.objects.get_or_create(
+                            name=row[self.INDEXES['village']].capitalize()
+                        )
+                        block, created = Block.objects.get_or_create(
+                            name=row[self.INDEXES['block']].capitalize(),
+                            education_district=district
+                        )
+                        cluster, created = Cluster.objects.get_or_create(
+                            name=row[self.INDEXES['cluster']].capitalize(),
+                            block=block
+                        )
+                        school, created = School.objects.get_or_create(
+                            name=row[self.INDEXES['school_name']],
+                            code=str(int(row[self.INDEXES['school_code']])),
+                            pincode=int(row[self.INDEXES['pincode']]),
+                            cluster=cluster,
+                            village=village
+                        )
+                        print "%s/%s: %s%% done." % (idx, sheet.nrows, (idx/float(sheet.nrows))*100)
+
+            except Exception as e:
+                print str(e)
