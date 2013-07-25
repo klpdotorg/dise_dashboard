19595bdb4bf7cdf1d815faf1fcc4f6ceaca6a468
Bibhas C Debnath <iambibhas@gmail.com>
2013-07-25 22:56:12 +0530

    modified script to accomodate yearly data

--- apps/common/management/commands/import_basic.py
+++ apps/common/management/commands/import_basic.py
@@ -8,7 +8,7 @@ from common.models import Cluster, Block, EducationDistrict, Village, State
 from django.conf import settings
 
 from common.models import Cluster, Block, Village, State, EducationDistrict
-from schools.models import School
+from schools.models import School, AcademicYear, YearlyData
 
 class Command(BaseCommand):
     args = '<filename filename ...>'
@@ -29,7 +29,7 @@ class Command(BaseCommand):
         'pincode': 6
     }
 
-    def process_row(self, row):
+    def process_row(self, row, year=None):
         district, created = EducationDistrict.objects.get_or_create(
             name=row[self.INDEXES['district']]
         )
@@ -49,11 +49,22 @@ class Command(BaseCommand):
             name=row[self.INDEXES['school_name']],
             code=str(int(row[self.INDEXES['school_code']])),
             pincode=int(row[self.INDEXES['pincode']]),
-            cluster=cluster,
-            village=village
         )
+        yearly_data, created = YearlyData.objects.get_or_create(
+            school=school,
+            academic_year=year
+        )
+        yearly_data.cluster = cluster
+        yearly_data.village = village
+        yearly_data.save()
 
     def handle(self, *args, **options):
+        year=None
+
+        if 'year' in options:
+            from_year, to_year = options.get('year').split('-')
+            year = AcademicYear.objects.get(from_year=from_year, to_year=to_year)
+
         for basic_data in args:
             full_path = os.path.join(settings.DATADUMP_ROOT, basic_data)
             try:
@@ -61,10 +72,9 @@ class Command(BaseCommand):
                 for sheet in fp.sheets():
                     for idx in range(1, sheet.nrows):
                         row = sheet.row_values(idx)
-                        if (not row[self.INDEXES['school_code']] or
-                            School.objects.filter(code=str(int(row[self.INDEXES['school_code']]))).count() > 0):
+                        if not row[self.INDEXES['school_code']]:
                             continue
-                        self.process_row(row)
+                        self.process_row(row, year)
 
                         if idx % 100 == 0:
                             print "%s/%s: %s%% done." % (idx, sheet.nrows, (idx/float(sheet.nrows))*100)
