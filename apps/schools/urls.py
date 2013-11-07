0ce07b0ef67dd18e27a85d154b4d9a4dd9defdb4
Bibhas <iambibhas@gmail.com>
2013-11-08 02:50:09 +0530

    pointing to new olap endpoint

--- apps/schools/urls.py
+++ apps/schools/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from schools.olap_views import OLAPEndPoint
 
 urlpatterns = patterns('',
-
+    url(r'^olap/$', OLAPEndPoint.as_view(), name='olap_endpoint'),
 )
--- dise_dashboard/urls.py
+++ dise_dashboard/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
 
     # url(r'^schools/', include('schools.urls')),
 
-    url(r'^api/v1/', include('schools.v1_urls')),
+    url(r'^api/v1/', include('schools.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
