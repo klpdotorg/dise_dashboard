4bae7c8856bc0953ae571371aa2be6a8f130fb19
Bibhas <iambibhas@gmail.com>
2013-12-20 12:35:34 +0530

    updated assets

--- assets/static/js/brain.js
+++ assets/static/js/brain.js
@@ -25,25 +25,31 @@ $(function(){
 
     $("#control_toggle").click(function(e){
         $("#controls").toggleClass("hidden_controls");
-        $("#control_toggle").toggleClass("flip");
-        var t = $("#control_toggle").parent().find("span.text");
+        $("#control_toggle_icon").toggleClass("flip");
+        var t = $("#control_toggle").find("span.text");
         var t_html = (t.html() === "Hide") ? "Show" : "Hide";
         t.html(t_html);
     });
 
 
-    // Breadcrumbs coloring and z-index
-    var class_array = ["a", "b", "c", "d", "e"]
-    $("ol.top-breadcrumbs li").each(function(i, e){
-        $(e).addClass(class_array[i]);
-    });
+    renderCrumbs([
+        ["Karnataka", "#"],
+        ["Bangalore", "#"],
+        ["HSR Layout", "#"]
+    ])
 
     // Sliders
     $(".range-slider").slider({
         range: true,
         min: 0,
         max: 30,
-        values: [10,20]
+        values: [10,20],
+        slide: function(e, s){
+            var high = $("#" + $(this).attr("data-high"));
+            var low = $("#" + $(this).attr("data-low"));
+            high.val(s.values[1]);
+            low.val(s.values[0]);
+        }
     });
 
     // Filter
@@ -80,3 +86,23 @@ $(function(){
         ]
     });
 });
+
+
+function renderCrumbs(bs){
+    var ol = $("ol.top-breadcrumbs");
+    ol.html('<li><a class="navbar-brand" href="#">DISE</a></li>');
+
+    for (var i in bs){
+        var b = bs[i];
+        var li = $("<li>");
+        var a = $("<a>").attr("href", b[1]).html(b[0]);
+        ol.append(li.append(a));
+    }
+
+
+    // Coloring and z-index
+    var class_array = ["a", "b", "c", "d", "e"]
+    ol.find("li").each(function(i, e){
+        $(e).addClass(class_array[i]);
+    });
+}
