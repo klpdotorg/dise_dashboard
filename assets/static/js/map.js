705028dd070a6aba2526aa4cf81fdb23ecbd6652
Sajjad Anwar <me@sajjad.in>
2013-12-26 12:16:21 +0530

    Custom attribution control

--- assets/static/js/map.js
+++ assets/static/js/map.js
@@ -6,13 +6,15 @@ var bangalore = L.latLng([12.9719,77.5937]);
 var bounds;
 
 // Initialise the map object.
-map = L.map('map-holder', {zoomControl: false, attributionControl: false}).setView(bangalore, 9);
+map = L.map('map-holder', {zoomControl: true, attributionControl: false}).setView(bangalore, 9);
 
 // Tile URL, Key and attribution.
 var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png',
 cloudmadeKey = '3c4ab920016a46d8ba10c88227539774',
-cloudmadeAttribution = '<a href="http://osm.org">OpenStreetMap</a>, <a href="http://cloudmade.com>CloudMade"</a>';
+cloudmadeAttribution = '<a href="http://osm.org">OpenStreetMap</a>, <a href="http://cloudmade.com">CloudMade</a>';
 
 // Create the Tile layer and add it to the map.
 var cloudmadeLayer = L.tileLayer(cloudmadeUrl, {key: cloudmadeKey}).addTo(map);
 
+// Custom attribution control.
+var attributionControl = L.control.attribution({position: 'bottomright', prefix: cloudmadeAttribution}).addTo(map);
