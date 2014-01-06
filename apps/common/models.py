06110efd8e0794fdcf06210d3d2c1f4d3928168a
Bibhas <iambibhas@gmail.com>
2014-01-06 16:46:37 +0530

    cleaned up old code base

--- apps/common/models.py
+++ apps/common/models.py
@@ -26,42 +26,42 @@ class BaseModel(models.Model):
         return super(BaseModel, self).save(*args, **kwargs)
 
 
-class Cluster(models.Model):
-    name = models.CharField(max_length=255)
-    block = models.ForeignKey('Block', blank=True, null=True)
+# class Cluster(models.Model):
+#     name = models.CharField(max_length=255)
+#     block = models.ForeignKey('Block', blank=True, null=True)
 
-    def __unicode__(self):
-        return u"%s" % self.name
-
-
-class Block(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    education_district = models.ForeignKey('EducationDistrict', blank=True, null=True)
-
-    def __unicode__(self):
-        return u"%s" % self.name
+#     def __unicode__(self):
+#         return u"%s" % self.name
 
 
-class EducationDistrict(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    state = models.ForeignKey('State', blank=True, null=True)
+# class Block(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
+#     education_district = models.ForeignKey('EducationDistrict', blank=True, null=True)
 
-    def __unicode__(self):
-        return u"%s" % self.name
-
-
-class Village(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __unicode__(self):
-        return u"%s" % self.name
-
-    class Meta:
-        ordering = ('name', )
+#     def __unicode__(self):
+#         return u"%s" % self.name
 
 
-class State(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+# class EducationDistrict(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
+#     state = models.ForeignKey('State', blank=True, null=True)
 
-    def __unicode__(self):
-        return u"%s" % self.name
+#     def __unicode__(self):
+#         return u"%s" % self.name
+
+
+# class Village(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
+
+#     def __unicode__(self):
+#         return u"%s" % self.name
+
+#     class Meta:
+#         ordering = ('name', )
+
+
+# class State(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
+
+#     def __unicode__(self):
+#         return u"%s" % self.name
--- apps/schools/admin.py
+++ apps/schools/admin.py
@@ -1,51 +1,51 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import *
-
-
-class BaseTabularAdmin(admin.TabularInline):
-    exclude = ("date_created", "date_modified",)
+# from .models import *
 
 
-class RoomTabularAdmin(BaseTabularAdmin):
-    model = Room
+# class BaseTabularAdmin(admin.TabularInline):
+#     exclude = ("date_created", "date_modified",)
 
 
-class ToiletTabularAdmin(BaseTabularAdmin):
-    model = Toilet
+# class RoomTabularAdmin(BaseTabularAdmin):
+#     model = Room
 
 
-class EnrolmentTabularAdmin(BaseTabularAdmin):
-    model = Enrolment
+# class ToiletTabularAdmin(BaseTabularAdmin):
+#     model = Toilet
 
 
-class TeacherCountTabularAdmin(BaseTabularAdmin):
-    model = TeacherCount
+# class EnrolmentTabularAdmin(BaseTabularAdmin):
+#     model = Enrolment
 
 
-class YearlyDataAdmin(admin.ModelAdmin):
-    raw_id_fields = ("academic_year", "school", "cluster", "village",)
-    readonly_fields = ("date_created", "date_modified",)
-    search_fields = ("school__name", "school__code")
-    list_display = (
-        "school", "academic_year", "area_type", "type"
-    )
-    inlines = (
-        RoomTabularAdmin,
-        ToiletTabularAdmin,
-        EnrolmentTabularAdmin,
-        TeacherCountTabularAdmin
-    )
-admin.site.register(YearlyData, YearlyDataAdmin)
+# class TeacherCountTabularAdmin(BaseTabularAdmin):
+#     model = TeacherCount
 
-admin.site.register([
-    School,
-    InstractionMedium,
-    SchoolManaagement,
-    SchoolCategory,
-    ResidentialType,
-    AcademicYear,
-    SchoolBuildingStatus,
-    DrinkingWaterSource,
-    BoundaryWallType
-])
+
+# class YearlyDataAdmin(admin.ModelAdmin):
+#     raw_id_fields = ("academic_year", "school", "cluster", "village",)
+#     readonly_fields = ("date_created", "date_modified",)
+#     search_fields = ("school__name", "school__code")
+#     list_display = (
+#         "school", "academic_year", "area_type", "type"
+#     )
+#     inlines = (
+#         RoomTabularAdmin,
+#         ToiletTabularAdmin,
+#         EnrolmentTabularAdmin,
+#         TeacherCountTabularAdmin
+#     )
+# admin.site.register(YearlyData, YearlyDataAdmin)
+
+# admin.site.register([
+#     School,
+#     InstractionMedium,
+#     SchoolManaagement,
+#     SchoolCategory,
+#     ResidentialType,
+#     AcademicYear,
+#     SchoolBuildingStatus,
+#     DrinkingWaterSource,
+#     BoundaryWallType
+# ])
