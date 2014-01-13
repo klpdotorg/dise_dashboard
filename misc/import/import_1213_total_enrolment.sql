c080ce9ff37917ebd8dce51300d1483af4d710c1
Bibhas <iambibhas@gmail.com>
2014-01-14 03:38:03 +0530

    added enrolment import script

---
+++ misc/import/import_1213_total_enrolment.sql
@@ -0,0 +1,17 @@
+ALTER TABLE dise_1213_basic_data
+ ADD COLUMN class1_total_enr_boys integer,
+ ADD COLUMN class2_total_enr_boys integer,
+ ADD COLUMN class3_total_enr_boys integer,
+ ADD COLUMN class4_total_enr_boys integer,
+ ADD COLUMN class5_total_enr_boys integer,
+ ADD COLUMN class6_total_enr_boys integer,
+ ADD COLUMN class7_total_enr_boys integer,
+ ADD COLUMN class8_total_enr_boys integer,
+ ADD COLUMN class1_total_enr_girls integer,
+ ADD COLUMN class2_total_enr_girls integer,
+ ADD COLUMN class3_total_enr_girls integer,
+ ADD COLUMN class4_total_enr_girls integer,
+ ADD COLUMN class5_total_enr_girls integer,
+ ADD COLUMN class6_total_enr_girls integer,
+ ADD COLUMN class7_total_enr_girls integer,
+ ADD COLUMN class8_total_enr_girls integer;
