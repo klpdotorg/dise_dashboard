446d68e13f905397f23b96d0edea99459915d131
Bibhas <iambibhas@gmail.com>
2014-01-20 15:48:24 +0530

    added about page template

--- dise_dashboard/urls.py
+++ dise_dashboard/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -6,6 +7,7 @@ from schools.views import HomeView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
     # url(r'^schools/', include('schools.urls')),
 
