2818531c21d9348032d63a6e5414a9d1a1881701
Bibhas <iambibhas@gmail.com>
2014-02-06 00:32:09 +0530

    appending project root to system path from wsgi

--- dise_dashboard/wsgi.py
+++ dise_dashboard/wsgi.py
@@ -14,9 +14,14 @@ framework.
 
 """
 import os
+import sys
 import newrelic.agent
 
-newrelic.agent.initialize('newrelic.ini')
+PROJECT_ROOT = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
+
+sys.path.append(PROJECT_ROOT)
+
+newrelic.agent.initialize(os.path.join(PROJECT_ROOT, 'newrelic.ini'))
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
