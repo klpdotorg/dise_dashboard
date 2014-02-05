b2398ac0f81438c83eae222cdcfd3f97a20f5ef5
Bibhas <iambibhas@gmail.com>
2014-02-05 20:03:15 +0530

    added newrelic to wsgi script

--- dise_dashboard/wsgi.py
+++ dise_dashboard/wsgi.py
@@ -14,7 +14,9 @@ framework.
 
 """
 import os
+import newrelic.agent
 
+newrelic.agent.initialize('newrelic.ini')
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
