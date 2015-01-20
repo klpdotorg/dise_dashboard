41e854207497dc4836e3bc2c608243a2a768d3ba
Bibhas <iambibhas@gmail.com>
2015-01-21 04:09:16 +0530

    added slug creating postgres function

---
+++ misc/slugfunction.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION getslug(texte varchar) RETURNS VARCHAR AS
+$$
+DECLARE
+    result varchar;
+BEGIN
+    result := replace(texte , 'æ', 'ae');
+    result := replace(result , 'œ', 'oe');
+    result := replace(result , '€', 'euros');
+    result := replace(result , '$', 'dollars');
+    result := replace(result , '£', 'pound');
+    result := replace(result , '¥', 'yen');
+    result := regexp_replace(translate(replace(lower(result), ' ', '-'),
+        'áàâãäåāăąÁÂÃÄÅĀĂĄèééêëēĕėęěĒĔĖĘĚìíîïìĩīĭÌÍÎÏÌĨĪĬóôõöōŏőÒÓÔÕÖŌŎŐùúûüũūŭůÙÚÛÜŨŪŬŮçÇÿ&,.ñÑ',
+        'aaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiooooooooooooooouuuuuuuuuuuuuuuuccy_--nn'), E'[^\\w -]', '', 'g');
+    result := regexp_replace(result, E'-+', '-', 'g');
+    result := trim(both '-' from result);
+    RETURN result;
+END;
+$$
+LANGUAGE PLPGSQL;
