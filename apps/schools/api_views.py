0639caaf836734896c5c3ad3378261356afff0c1
Bibhas <iambibhas@gmail.com>
2015-01-21 12:50:56 +0530

    added schools list urls to API root

--- apps/schools/api_views.py
+++ apps/schools/api_views.py
@@ -127,6 +127,8 @@ class AggregationInfoView(AggregationBaseView, generics.RetrieveAPIView):
 
 
 class AggregationSchoolListView(SchoolApiBaseView, generics.ListAPIView):
+    """Lists all the schools in the given aggregated entity_obj
+    """
     def get_queryset(self):
         schools = super(AggregationSchoolListView, self).get_queryset()
         slug = self.kwargs.get('slug', None)
@@ -171,5 +173,50 @@ def api_root(request, format=None):
         endpoints['%s List' % entity.title()] = reverse(
             'api_entity_list', request=request, args=['12-13', entity]
         )
+
+    endpoints['Cluster Info'] = reverse(
+        'api_entity_info', request=request,
+        args=['12-13', 'cluster', 'davanageren-avaragere']
+    )
+    endpoints['Schools in Cluster'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'cluster', 'davanageren-avaragere']
+    )
+    endpoints['Block Info'] = reverse(
+        'api_entity_info', request=request,
+        args=['12-13', 'block', 'gulbarga-afzalpur']
+    )
+    endpoints['Schools in Block'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'block', 'gulbarga-afzalpur']
+    )
+    endpoints['Scholls in District'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'district', 'bagalkot']
+    )
+    endpoints['Assembly Info'] = reverse(
+        'api_entity_info', request=request,
+        args=['12-13', 'assembly', 'bhadravati']
+    )
+    endpoints['Schools in Assembly'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'assembly', 'bhadravati']
+    )
+    endpoints['Parliament Info'] = reverse(
+        'api_entity_info', request=request,
+        args=['12-13', 'parliament', 'bagalkot']
+    )
+    endpoints['Schools in Parliament'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'parliament', 'bagalkot']
+    )
+    endpoints['Pincode Info'] = reverse(
+        'api_entity_info', request=request,
+        args=['12-13', 'pincode', '560044']
+    )
+    endpoints['Schools in Pincode'] = reverse(
+        'api_entity_school_list', request=request,
+        args=['12-13', 'pincode', '560044']
+    )
 
     return Response(endpoints)
