45be6b5bb2a5da6bce53df5e4de6075430553a7e
Bibhas <iambibhas@gmail.com>
2015-09-07 12:41:21 +0530

    added 1415 cleanup sql

---
+++ misc/cleanup.sql
@@ -0,0 +1,5 @@
+UPDATE dise_1011_basic_data SET school_name=regexp_replace(school_name, '\s+', ' ', 'g') WHERE school_name LIKE E'%  %';
+UPDATE dise_1112_basic_data SET school_name=regexp_replace(school_name, '\s+', ' ', 'g') WHERE school_name LIKE E'%  %';
+UPDATE dise_1213_basic_data SET school_name=regexp_replace(school_name, '\s+', ' ', 'g') WHERE school_name LIKE E'%  %';
+UPDATE dise_1314_basic_data SET school_name=regexp_replace(school_name, '\s+', ' ', 'g') WHERE school_name LIKE E'%  %';
+UPDATE dise_1415_basic_data SET school_name=regexp_replace(school_name, '\s+', ' ', 'g') WHERE school_name LIKE E'%  %';
