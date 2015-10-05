fa6391a22ef571d1024e264ed02dc58f2fe6a293
Bibhas <iambibhas@gmail.com>
2015-10-05 15:25:40 +0530

    added some tests

--- apps/schools/tests.py
+++ apps/schools/tests.py
@@ -4,30 +4,47 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
-from django.test import TestCase
-from django.test.client import RequestFactory
-from django.test.client import Client
-import json
+from rest_framework.test import APITestCase, APIClient
+from schools.api_views import api_root
+from django.core.urlresolvers import reverse
 
 
-class EndpointTest(TestCase):
+class EndpointTest(APITestCase):
     def setUp(self):
-        # Every test needs access to the request factory.
-        self.client = Client()
+        # Using the standard RequestFactory API to create a form POST request
+        self.client = APIClient()
+        self.session = '14-15'
 
-    def test_endpoints(self):
+    def test_root(self):
         """
         Just for testing that things are OK
         """
-        response = self.client.get('/api/')
+        response = self.client.get(reverse('api_root'))
         self.assertEqual(response.status_code, 200)
+        # no key named 'error'
+        self.assertNotIn('error', response.data.keys())
 
-        print 'Testing that all the endpoints are alive'
-        for endpoint in response.data.values():
-            print endpoint
-            eresp = self.client.get(endpoint)
-            self.assertEqual(
-                eresp.status_code, 200,
-                'Found %s during %s' % (eresp.status_code, endpoint)
-            )
+    def test_search(self):
+        """
+        Search all the things
+        """
+        response = self.client.get(
+            reverse('api_search', args=(self.session,)) + '?type=all&query=pura'
+        )
+        # result is a list of entity types
+        self.assertIsInstance(response.data, list)
+        # there are 5 types of entities right now
+        self.assertEqual(len(response.data), 5)
+
+
+    def test_school_list(self):
+        """
+        List of schools
+        """
+        response = self.client.get(
+            reverse('api_school_list', args=(self.session,)) + '?management=govt&area=urban'
+        )
+        # no error
+        self.assertNotIn('error', response.data.keys())
+        # there are more than 0 results
+        self.assertNotEqual(response.data.get('count'), 0)
