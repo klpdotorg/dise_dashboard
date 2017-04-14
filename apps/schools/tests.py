da9b10cc9c7cbe15d3282f6c519d4464bbfb2c25
Bibhas <iambibhas@gmail.com>
2017-04-14 10:05:51 +0530

    added some more tests

--- apps/schools/tests.py
+++ apps/schools/tests.py
@@ -13,7 +13,8 @@ class EndpointTest(APITestCase):
     def setUp(self):
         # Using the standard RequestFactory API to create a form POST request
         self.client = APIClient()
-        self.session = '14-15'
+        self.session = '15-16'
+        self.valid_dise_code = 29230801501
 
     def test_root(self):
         """
@@ -36,7 +37,6 @@ class EndpointTest(APITestCase):
         # there are 5 types of entities right now
         self.assertEqual(len(response.data), 5)
 
-
     def test_school_list(self):
         """
         List of schools
@@ -47,4 +47,48 @@ class EndpointTest(APITestCase):
         # no error
         self.assertNotIn('error', response.data.keys())
         # there are more than 0 results
+        self.assertNotEqual(response.data.get('count'), 0)
+
+    def test_invalid_school(self):
+        """
+        Invalid DISE code - 2923080151
+        """
+        response = self.client.get(
+            reverse('api_school_info', args=(self.session, 123456))
+        )
+        # got error
+        self.assertEqual(response.data.get('detail'), "Not found.")
+
+    def test_valid_school(self):
+        """
+        Valid DISE code - 29230801501
+        """
+        response = self.client.get(
+            reverse('api_school_info', args=(self.session, self.valid_dise_code))
+        )
+        # got error
+        self.assertEqual(response.data.get('id'), self.valid_dise_code)
+
+    def test_aggregate_list(self):
+        response = self.client.get(reverse('api_entity_list', args=(self.session, 'district')))
+        self.assertNotEqual(response.data.get('count'), 0)
+
+    def test_aggregate_info(self):
+        response = self.client.get(reverse('api_entity_info', args=(self.session, 'district', 'bagalkot')))
+        self.assertNotEqual(response.data.get('properties').get('sum_has_toilet'), 0)
+
+    def test_aggregate_school_list(self):
+        response = self.client.get(reverse('api_entity_school_list', args=(self.session, 'district', 'bagalkot')))
+        self.assertNotEqual(response.data.get('count'), 0)
+
+    def test_clusters_in_block(self):
+        response = self.client.get(reverse('api_clusters_in_block', args=(self.session, 'block', 'bagalkot-badami')))
+        self.assertNotEqual(response.data.get('count'), 0)
+
+    def test_clusters_in_district(self):
+        response = self.client.get(reverse('api_clusters_in_district', args=(self.session, 'district', 'bagalkot')))
+        self.assertNotEqual(response.data.get('count'), 0)
+
+    def test_blocks_in_district(self):
+        response = self.client.get(reverse('api_blocks_in_district', args=(self.session, 'district', 'bagalkot')))
         self.assertNotEqual(response.data.get('count'), 0)
