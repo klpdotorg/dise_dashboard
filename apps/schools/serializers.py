b0bc2b4d3b18e8100ff1562a3a9b4e2afd29e5d2
Subha srinivasan <subha@klp.org.in>
2020-06-23 17:11:23 +0530

    reverting a fix

--- apps/schools/serializers.py
+++ apps/schools/serializers.py
@@ -161,7 +161,7 @@ class DistrictSerializer(AggregationBaseSerializer):
     class Meta:
         model = DistrictAggregations
         geo_field = 'centroid'
-        id_field = 'district'
+        pk_field = 'district'
         fields = [field.name for field in DistrictAggregations._meta.get_fields()] + [
             'entity_type', 'popup_content', 'medium_of_instructions', 'school_categories'
         ]
